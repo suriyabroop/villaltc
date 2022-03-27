60a2df1f3f3053ce3bf56c459d7d00043af4fa29
suriyabroop <98744069+suriyabroop@users.noreply.github.com>
2022-03-27 14:06:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,29 @@
+module.exports = { 
+ 
+mongoLink:'mongodb+srv://tnvillanyt:suriyaskp12@cluster0.tzm8c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',// Database Link Of MongoDB 
+ 
+apikey:'EamGXayT3yLF0tUK',// Coinbase API Key 
+ 
+secretkey:'QIyYjkevBj4L0WIxB3Ha252kUkYbUG6l',// Coinbase 2nd Key 
+ 
+bot_token:'5068366893:AAFVOq0vaRWZK9zm1xbOa1XPoMpFzW9_wqg',// Bot Token 
+ 
+bot_name:'Litecoin_KINGBOT',// Bot Username Without @ 
+ 
+bot_admin:1996486622,// Admin Telegram Id 
+ 
+channelsList : ['@BranchAirdrops','@OfficialEarthQuake','@MontageDrops','@DailyUpdate_R','@TNG_TO_MOON'],// Channels List 
+ 
+channelscheck : ['@BranchAirdrops','@OfficialEarthQuake','@MontageDrops','@DailyUpdate_R','@TNG_TO_MOON'],// Check Channels List 
+ 
+reffer_bonus:0.00001,// Refferal Bonus Amount 
+ 
+min_wd:0.00003,// Min Withdrawal Amount 
+ 
+daily_bonus:0.000001,// Daily Bonus Amount 
+ 
+currency:'LTC', // Bot Currency 
+ 
+payment_channel:'@TNG_TO_MOON'// Payment Channel Username 
+ 
+}
